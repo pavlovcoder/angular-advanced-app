b4fc752224f945bbf5cf514beb9bc979d90f9038
Vladimir Pavlov <w.pavlov@hit-kody.com.pl>
2019-03-21 21:55:52 +0100

    v1.0.0-beta.1

---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { PipesComponent } from './pipes/pipes.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const appRoutes: Routes = [
+  { path: 'pipes', component: PipesComponent },
+  { path: '', redirectTo: '/pipes', pathMatch: 'full' }
+  { path: '**', component: PageNotFoundComponent },
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PipesComponent,
+    PageNotFoundComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(
+      appRoutes, 
+      { enableTracing: true }
+    )
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
