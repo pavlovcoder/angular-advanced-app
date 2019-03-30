df0b0d0bd24e12b4897df8889c8df09cf25f037a
Vladimir Pavlov <pavlovcoder@gmail.com>
2019-03-30 23:05:28 +0100

    v1.0.0-beta.3

--- src/app/pipes/pipes.component.ts
+++ src/app/pipes/pipes.component.ts
@@ -8,6 +8,15 @@ import { Component, OnInit } from '@angular/core';
 export class PipesComponent implements OnInit {
 
   public birthday: Date = new Date(1996, 2, 5);
+  public toggle: boolean = true;
+
+  get format() {
+    return this.toggle ? 'shortDate' : 'fullDate';
+  }
+
+  public toggleFormat(): void {
+    this.toggle = !this.toggle;
+  }
 
   constructor() { }
 
